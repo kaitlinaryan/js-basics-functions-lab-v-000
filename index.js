423cb8386db4d0b915ac76c1adf441b50b14d6c0
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-19 18:35:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,15 +20,16 @@ function distanceTravelledInFeet (start, destination) {
  }
  }
 
- function calculatesFarePrice (start, destination){
-   const distance = distanceTravelledInFeet(start, destination);
-   if (distance <= 400) {
-   return 0;
-   } else if (distance > 400 && <= 2000) {
-   return distance * .02;
-   } else if (distance > 2000 && distance < 2500) {
-   return 25;
-   } else {
-   return 'cannot travel that far.';
-   }
- }
+ function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * distance;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
